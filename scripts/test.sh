1f3cdd2b44d63d43ec5b45d59a22c24e389ecddf
Jonathan Jouty <jonathan@scrive.com>
2020-06-30 16:06:00 -0500

    Test against scrive_api.md

--- scripts/test.sh
+++ scripts/test.sh
@@ -15,6 +15,10 @@ echo "# basic.yaml matches"
 ./main.js $input/basic.yaml > $output/basic.md
 diff $check/basic.md $output/basic.md
 
+echo "# Scrive API docs match"
+./main.js $input/scrive-apidocs/scrive_api.yaml > $output/scrive_api.md
+diff $check/scrive_api.md $output/scrive_api.md
+
 # TODO add more example conversions
 
 # TODO run with different flags
