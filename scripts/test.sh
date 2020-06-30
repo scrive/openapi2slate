0d01af86e807ce13aa30f73c8e533e9cbd2a0282
Jonathan Jouty <jonathan@scrive.com>
2020-06-30 15:58:44 -0500

    Add basic spec for testing

--- scripts/test.sh
+++ scripts/test.sh
@@ -2,6 +2,7 @@
 
 echo "Running comparison tests"
 
+input=./test/input
 check=./test/check
 output=./test/output
 mkdir -p $output
@@ -10,5 +11,10 @@ echo "# Help text matches"
 ./main.js --help > $output/help
 diff $check/help $output/help
 
-# TODO add some example conversions
+echo "# basic.yaml matches"
+./main.js $input/basic.yaml > $output/basic.md
+diff $check/basic.md $output/basic.md
+
+# TODO add more example conversions
+
 # TODO run with different flags
